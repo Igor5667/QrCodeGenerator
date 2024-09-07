f5a20d5386cf650315176a04ed9b322841458a40
Igor5567 <igidod@gmail.com>
2024-09-07 15:35:55 +0200

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,36 @@ function App() {
   const [inputValue, setInputValue] = useState<string>("");
 
   const getQrCode = () => {
-    let qr = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
-    setQrImg(qr);
+    let link = inputValue.trim();
+    if (link.length < 1) return;
+    setQrImg(
+      `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${link}`
+    );
   };
 
   return (
-    <div className="text-red-600">
-      <h1>Lets get some QrCode</h1>
-      <button onClick={getQrCode}>Click</button>
-      <input
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        type="text"
-        placeholder="paste link"
-      />
-      {qrImg && <img src={qrImg} alt="qrCode" />}
+    <div className="flex h-screen w-screen justify-center items-center bg-background-gradient">
+      <div className="w-2/3 h-[90%] flex flex-col items-center gap-5 bg-background-card-gradient text-white border border-slate-400 rounded-3xl py-10 shadow-xl">
+        <h1 className="text-3xl mb-6 w-[90%] text-center">
+          Lets make some QR Codes !
+        </h1>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          type="text"
+          placeholder="paste link here ..."
+          className="w-2/3 bg-transparent border-2 border-slate-400 rounded-full px-5 py-1 focus:outline-none text-lg"
+        />
+        <button
+          onClick={getQrCode}
+          className="border py-2 px-10 rounded-2xl bg-white text-black"
+        >
+          GENERATE
+        </button>
+        <div className="flex-1 aspect-square border rounded-2xl p-5 mt-5">
+          {qrImg && <img src={qrImg} alt="qrCode" width="100%" />}
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "background-gradient": "linear-gradient(61deg, #44471d, #202922);",
+        "background-card-gradient":
+          "linear-gradient(61deg, rgba(0,0,0,0.4), rgba(0,0,0,0.1))",
+      },
+    },
   },
   plugins: [],
 };
